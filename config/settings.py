0c6e59737a1fcda654dc2c49b9954e7a28403af2
vgrillot <grivin@gmail.com>
2021-11-16 22:17:37 +0100

    use my public ip as debug ip

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html
 # https://docs.djangoproject.com/en/dev/ref/settings/#internal-ips
 # !!!TODO:remove heroku for prod !
-INTERNAL_IPS = ['127.0.0.1', 'repete-online.herokuapp.com']
+INTERNAL_IPS = ['127.0.0.1', '91.172.197.92']
 
 # CUSTOM USER MODEL CONFIGS
 # ------------------------------------------------------------------------------
